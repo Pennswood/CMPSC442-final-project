07710fd42aaa055c8d04688884ee6ca06fd836c1
nxa5282 <90064658+nxa5282@users.noreply.github.com>
2023-04-17 18:04:16 -0400

    Update additional_todo.py

--- additional_todo.py
+++ additional_todo.py
@@ -1,5 +1,10 @@
 def todo(Lesson, lesson_list):
     people = ["Sophia", "Mark", "Ahmed", "Normen", "Noor"]
     for p in people:
-        lesson_list.append(Lesson(len(lesson_list), "Lunch", p, "LUNCH"))
+        lesson_list.append(Lesson(len(lesson_list), "Meal", p, "MEAL")) #combine lunch/dinner/breakfast
+        lesson_list.append(Lesson(len(lesson_list), "Sleep", p, "SLEEP")) #or rest
+        lesson_list.append(Lesson(len(lesson_list), "Study", p, "STUDY"))
+        lesson_list.append(Lesson(len(lesson_list), "Exercise", p, "EXERCISE"))
+        lesson_list.append(Lesson(len(lesson_list), "Class", p, "CLASS"))
+        lesson_list.append(Lesson(len(lesson_list), "Free Time", p, "FREE TIME")) #lowest priority? 
     pass
